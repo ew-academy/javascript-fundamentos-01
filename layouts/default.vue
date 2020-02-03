29686f48c7b99a12adef291dd4c96eb7e6973007
Erick Wendel <erick.workspace@gmail.com>
2020-02-03 01:22:38 -0300

    add hotmart and ga
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,25 +5,35 @@
 </template>
 
 <script>
-export default {};
-</script>
 
-<script id="hotmart_launcher_script">
-  (function(l,a,u,n,c,h,e,r){l['HotmartLauncherObject']=c;l[c]=l[c]||function(){
-  (l[c].q=l[c].q||[]).push(arguments)},l[c].l=1*new Date();h=a.createElement(u),
-  e=a.getElementsByTagName(u)[0];h.async=1;h.src=n;e.parentNode.insertBefore(h,e)
-  })(window,document,'script','//launcher.hotmart.com/launcher.js','hot');
+function addHotMart() {
+  const text = "(function(l,a,u,n,c,h,e,r){l['HotmartLauncherObject']=c;l[c]=l[c]||function(){(l[c].q=l[c].q||[]).push(arguments)},l[c].l=1*new Date();h=a.createElement(u),e=a.getElementsByTagName(u)[0];h.async=1;h.src=n;e.parentNode.insertBefore(h,e)})(window,document,'script','//launcher.hotmart.com/launcher.js','hot');"
+
+  const script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.innerText = text;
+  script.setAttribute('id', `hotmart_launcher_script`);
+  document.getElementsByTagName('head')[0].appendChild(script);
 
   hot('account','8fd69c75-8b5e-39d1-acc8-b7be4f3c396a');
-</script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157528209-1"></script>
-<script>
+
+}
+
+function addGa() {
+
+  const script = document.createElement('script');
+  script.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-157528209-1');
+  script.setAttribute('type', 'text/javascript');
+  document.getElementsByTagName('head')[0].appendChild(script);
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-157528209-1');
+}
+
+addGa()
+addHotMart()
+export default {};
 </script>
 
 <style lang="sass">
