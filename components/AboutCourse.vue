ca2a48431e733a1d6583e36f403a06b49316b4f0
Erick Wendel <erick.workspace@gmail.com>
2020-02-02 22:30:33 -0300

    change about text
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- components/AboutCourse.vue
+++ components/AboutCourse.vue
@@ -3,15 +3,16 @@
     <div class="container AboutCourseContainer">
       <StyledTitle margin="0px 0px 30px 0px">Sobre o curso</StyledTitle>
       <p>
-        Varrendo a internet, encontramos diversos treinamentos sobre  <strong>Fundamentos</strong>  de  <strong>Javascript</strong>. Mas na maioria das vezes estes conteúdos  <strong>são focados na mesma coisa</strong>!  Este treinamento foi formatado junto à comunidade brasileira de Javascript e Node.js, mostrar os fundamentos da linguagem apresentando seus principais benefícios e tudo isso <strong>na prática</strong>.
+        Para você que está com <strong>dificuldades</strong> para <strong>aprender a programar</strong>, <strong>fazendo faculdade</strong>, tem curiosidade em aprender Javascript ou deseja aprender a sua primeira linguagem de programação na <strong>prática</strong>, sem lero lero, encontrou o treinamento que estava procurando!.
       </p>
       <br>
       <p>
-        Este treinamento foi  <strong>desenvolvido</strong>  sob medida a partir de uma pesquisa sobre o que a  <strong>comunidade brasileira mais necessitava</strong>, suas principais  <strong>dificuldades</strong>  e  <strong>desafios</strong>. Neste curso você aprenderá desde o básico de Javascript, ciclo de vida, variáveis, tipos de dados, manipulação de listas à uma aplicação real. Ao fim do curso você receberá um belíssimo certificado e terá acesso direto ao instrutor via nosso grupo oficial no facebook.
+        Este treinamento foi criado junto à <strong>comunidade</strong> brasileira de Javascript e Node.js, a partir de uma pesquisa sobre o que <strong>mais necessitavam</strong>, suas principais <strong>dificuldades</strong> e <strong>desafios</strong>. 
       </p>
       <br>
       <p>
-        Para você que está com <strong>dificuldades</strong> em <strong>lingugagens de programação</strong>, <strong>fazendo faculdade</strong>, tem curiosidade em aprender <strong>Javascript</strong> ou deseja aprender a sua primeira linguagem de programação na prática, sem <strong>lero lero</strong>, aprendendo poder da linguagem mais popular do mundo, certamente este treinamento é para <strong>você</strong>
+        Você aprenderá desde o <strong>básico de Javascript</strong>, ciclo de vida, variáveis, tipos de dados, manipulação de listas à uma <strong>aplicação real</strong>. Ao fim do curso você receberá um belíssimo <strong>certificado</strong> e terá <strong>acesso direto ao instrutor</strong> via nosso grupo oficial no Facebook.
+
       </p>
      
 
